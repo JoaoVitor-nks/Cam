9a3e472934c1a7e80791e5e73fb625797b7b3070
JoaoVitor-nks <161898276+JoaoVitor-nks@users.noreply.github.com>
2024-12-10 14:02:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-import streamlit as st
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, ClientSettings
-
-# Configurações para WebRTC
-WEBRTC_CLIENT_SETTINGS = ClientSettings(
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-    media_stream_constraints={"video": True, "audio": False},
-)
-
-st.title("Teste de Câmera")
-
-st.markdown(
-    """
-    - Certifique-se de conceder permissões para usar a câmera.
-    - Caso não veja o vídeo, tente em outro navegador ou verifique os logs.
-    """
-)
-
-# Captura e exibe o feed de vídeo
 webrtc_streamer(
-    key="camera-test",
-    mode=WebRtcMode.SENDRECV,  # Envia e recebe o feed
-    client_settings=WEBRTC_CLIENT_SETTINGS,
+    key="camera-test"
 )
