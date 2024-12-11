f4fb827b05e761150386f2a6e4204f073f787645
JoaoVitor-nks <161898276+JoaoVitor-nks@users.noreply.github.com>
2024-12-11 14:15:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 import cv2
 import streamlit as st
-import numpy as np
-import tempfile
 
-cap = cv2.VideoCapture(1)
+# Configurações de captura de vídeo
+cap = cv2.VideoCapture(1)  # Use 0 para a câmera padrão, 1 para outra câmera
 
-st.title('Projeto Cam')
+st.title("Projeto Cam")
 
+# Placeholder para exibir o feed
 frame_placeholder = st.empty()
 
-stop_button = st.button('Stop')
+# Botão para parar o vídeo
+stop_button = st.button("Stop")
 
-while cap.isOpened() and not stop_button:
-
+# Loop de captura de vídeo
+while not stop_button:
     ret, frame = cap.read()
 
     if not ret:
-        st.write("O video acabou")
+        st.write("Não foi possível capturar o vídeo.")
         break
 
+    # Converte o frame para RGB
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    frame_placeholder.image(frame, channels='RGB')
+    # Exibe o frame no Streamlit
+    frame_placeholder.image(frame, channels="RGB")
 
-    if cv2.waitKey(1) & 0xFF == ord('q') or stop_button:
-        break
-
-
+# Libera os recursos da câmera
 cap.release()
-cv2.destroyAllWindows()
