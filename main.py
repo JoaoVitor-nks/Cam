c6db50a512d04eb153d2433406420edc75f36a67
JoaoVitor-nks <161898276+JoaoVitor-nks@users.noreply.github.com>
2024-12-11 14:18:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
 import cv2
-import streamlit as st
-import numpy as np
-import tempfile
-
 
 cap = cv2.VideoCapture(0)
 
-st.title('Projeto Cam')
-
-frame_placeholder = st.empty()
-
-stop_button = st.button('Stop')
-
-
-while cap.isOpened() and not stop_button:
-
-    ret, frame = cap.read()
-
-    if not ret:
-        st.write("O video acabou")
-        break
-
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    frame_placeholder.image(frame, channels='RGB')
-
-    if cv2.waitKey(1) & 0xFF == ord('q') or stop_button:
-        break
-
+if not cap.isOpened():
+    print("Não foi possível acessar a câmera.")
+else:
+    print("Câmera acessada com sucesso!")
 
 cap.release()
-
